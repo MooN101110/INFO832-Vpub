50d01246081e0058617ae2a14d7c441b927031de
Louna Camas <lounacamas@gmail.com>
2025-02-26 11:18:17 +0100

    remove d'import

--- src/test/java/fr/usmb/vpub/CaveTest.java
+++ src/test/java/fr/usmb/vpub/CaveTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CaveTest {
     private Cave cave;
 
